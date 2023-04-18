70bda1284468c11a1e8942da3c783d6110f79b02
meanwhile7 <abhijit793001@gmail.com>
2023-04-18 16:44:16 +0530

    generative ui done

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const input2 = document.getElementById("num-lift");
 const myButton = document.getElementById("btn");
 
 function checkInputs() {
-  if (input1.value.length > 0 && input2.value.length > 0) {
-    myButton.disabled = false;
-    myButton.addEventListener("click", function () {
-      container.style.display = "none";
-    });
-  } else {
+  if(input1.value.length < 0 || input2.value.length < 0){
     myButton.disabled = true;
+    alert("negative number not allowed")
+  } else {
+    if (input1.value.length > 0 && input2.value.length > 0) {
+      myButton.disabled = false;
+      myButton.addEventListener("click", function () {
+        container.style.display = "none";
+      });
+    } else {
+      myButton.disabled = true;
+    }
   }
+  
 }
 input1.addEventListener("input", checkInputs);
 input2.addEventListener("input", checkInputs);
@@ -24,11 +30,46 @@ input1.addEventListener("change", (e) => {
   //store value of
   va = e.target.value;
 });
-function returnVa() {
+    
+function createFloor() {
+  const main = document.querySelector(".floor-box");
+
   for (i = 0; i < va; i++) {
-    const main = document.querySelector(".floor-box");
     const sec = document.createElement("section");
+    const div = document.createElement("div");
+    const div2 = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const bnt_up = document.createElement("button");
+    const bnt_down = document.createElement("button");
     sec.classList.add("floor");
+    div.classList.add("floor-section");
+    div2.classList.add("btn-section");
+    h1.textContent = (`floor ${i + 1}`);
+    bnt_up.classList.add("btn");
+    bnt_down.classList.add("btn");
+    bnt_up.textContent=("Up");
+    bnt_down.textContent=("Down");
     main.appendChild(sec);
+    sec.appendChild(div);
+    div.appendChild(div2);
+    div2.appendChild(h1);
+    div2.appendChild(bnt_up);
+    div2.appendChild(bnt_down);
+
+    for (j = 0; j < input2.value; j++) {
+      if (i === 0) {
+        const lift = document.createElement("div");
+        const left = document.createElement("span");
+        const right = document.createElement("span");
+        lift.classList.add("lift");
+        left.classList.add("left");
+        right.classList.add("right");
+        lift.appendChild(left);
+        lift.appendChild(right);
+        div.appendChild(lift);
+      }
+    }
   }
 }
+
+
