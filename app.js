b78bd821b718f2c3140233efd61e667099eb6deb
meanwhile7 <abhijit793001@gmail.com>
2023-04-18 11:41:26 +0530

    indent

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-// for checking the input element 
+// Conditions of input box //
 
 const input1 = document.getElementById("num-floor");
 const input2 = document.getElementById("num-lift");
@@ -8,36 +7,28 @@ const myButton = document.getElementById("btn");
 function checkInputs() {
   if (input1.value.length > 0 && input2.value.length > 0) {
     myButton.disabled = false;
-    myButton.addEventListener("click", function(){
-        container.style.display="none"
-    })
+    myButton.addEventListener("click", function () {
+      container.style.display = "none";
+    });
   } else {
     myButton.disabled = true;
   }
 }
-
-
-
 input1.addEventListener("input", checkInputs);
 input2.addEventListener("input", checkInputs);
 
 //
 
-let va = 0
-input1.addEventListener('change',(e)=>{        //store value of 
-    va = e.target.value
-})
-    function returnVa(){
-        for (i = 0; i< va ;i++){
-            const main = document.querySelector(".floor-box")
-            const sec = document.createElement("section")
-            sec.classList.add("floor");
-            main.appendChild(sec)
-        }
-        
-        
-    } 
-    
-    
-  
-
+let va = 0;
+input1.addEventListener("change", (e) => {
+  //store value of
+  va = e.target.value;
+});
+function returnVa() {
+  for (i = 0; i < va; i++) {
+    const main = document.querySelector(".floor-box");
+    const sec = document.createElement("section");
+    sec.classList.add("floor");
+    main.appendChild(sec);
+  }
+}
