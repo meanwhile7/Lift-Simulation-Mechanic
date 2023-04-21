62ff0e6e3ba1674133002559b35c955dbb614d7e
meanwhile7 <abhijit793001@gmail.com>
2023-04-21 19:22:36 +0530

    lift movement

--- app.js
+++ app.js
@@ -4,51 +4,64 @@ const input1 = document.getElementById("num-floor");
 const input2 = document.getElementById("num-lift");
 const myButton = document.getElementById("btn");
 
+
 function checkInputs() {
-  if(input1.value.length < 0 || input2.value.length < 0){
+  input1.addEventListener("input", checkInputs);
+  input2.addEventListener("input", checkInputs);
+  if (input1.value < 0 || input2.value < 0) {
     myButton.disabled = true;
-    alert("negative number not allowed")
+    alert("negative number not allowed");
+    location.reload();
   } else {
-    if (input1.value.length > 0 && input2.value.length > 0) {
-      myButton.disabled = false;
-      myButton.addEventListener("click", function () {
-        container.style.display = "none";
-      });
+    if (input1.value > 0 && input2.value > 0) {
+      container.style.display = "none";
+      createFloor();
     } else {
       myButton.disabled = true;
     }
   }
-  
 }
-input1.addEventListener("input", checkInputs);
-input2.addEventListener("input", checkInputs);
 
-//
+function onClickButton() {
+  
+  checkInputs();
+}
 
-let va = 0;
-input1.addEventListener("change", (e) => {
-  //store value of
-  va = e.target.value;
-});
-    
+myButton.addEventListener("click", onClickButton);
+
+
+
+// trying different values
+
 function createFloor() {
   const main = document.querySelector(".floor-box");
 
-  for (i = 0; i < va; i++) {
+  for (i = 0; i < input1.value ; i++) {
+
+    // create back button 
+    if (i==0){
+      const bac = document.createElement("input")
+      bac.classList.add("bac-btn")
+      main.appendChild(bac)
+    }
     const sec = document.createElement("section");
     const div = document.createElement("div");
     const div2 = document.createElement("div");
     const h1 = document.createElement("h1");
     const bnt_up = document.createElement("button");
     const bnt_down = document.createElement("button");
+    
     sec.classList.add("floor");
     div.classList.add("floor-section");
     div2.classList.add("btn-section");
-    h1.textContent = (`floor ${i + 1}`);
+    h1.textContent = `floor ${i + 1}`;
+    bnt_up.setAttribute("id",`up-${i+1}`)
+    bnt_down.setAttribute("id",`down-${i+1}`)
     bnt_up.classList.add("btn");
     bnt_down.classList.add("btn");
-    bnt_up.textContent=("Up");
-    bnt_down.textContent=("Down");
+    bnt_up.textContent = "Up";
+    bnt_down.textContent = "Down";
+    
     main.appendChild(sec);
     sec.appendChild(div);
     div.appendChild(div2);
@@ -62,6 +75,7 @@ function createFloor() {
         const left = document.createElement("span");
         const right = document.createElement("span");
         lift.classList.add("lift");
+        lift.setAttribute("id",`lift-${j+1}`)
         left.classList.add("left");
         right.classList.add("right");
         lift.appendChild(left);
@@ -70,6 +84,38 @@ function createFloor() {
       }
     }
   }
+    const upbtn = document.getElementById("up-2")
+    upbtn.addEventListener("click",function() {
+    const lift = document.querySelector('.lift');
+    moveLiftUp();
+    })
 }
+//   for (r=0 ; r < input1.value ; r++){
+
+//     const upbtn = document.getElementById(`up-${r}`)
+//     upbtn.addEventListener("click",function() {
+//     const lift = document.querySelector('.lift');
+//     console.log("upbtn")
+//     const rec = lift.getBoundingClientRect();
+//     moveLiftUp();
+//   // console.log(rec)
+// })
+//  
+    
 
 
+// const floorButtons = document.querySelectorAll('.btn');
+
+// // Attach event listener to each button
+// btn.forEach(btn => {
+//  btn.addEventListener('click', moveLiftUp);
+// });
+
+//moving the lift
+
+function moveLiftUp() {
+
+  const lift = document.querySelector('.lift');
+  lift.style.transform = "translateY(-180px)"
+
+}
