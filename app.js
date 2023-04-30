9d64ffeaa7f6ca006231948a370f4903686ec2bb
meanwhile7 <abhijit793001@gmail.com>
2023-04-30 12:31:38 +0530

    major changes

--- app.js
+++ app.js
@@ -29,125 +29,114 @@ myButton.addEventListener("click", checkInputs());
 
 function createFloor() {
   const main = document.querySelector(".floor-box");
-  for (i = 0; i < input1.value ; i++) {
+  for (let i = 0; i < input1.value; i++) {
 
     // create back button 
-    if (i==0){
-      const bac = document.createElement("button")
-      bac.textContent = "Back"
-      bac.classList.add("bac-btn")
-      main.appendChild(bac)
+    if (i === 0) {
+      const bac = document.createElement("button");
+      bac.textContent = "Back";
+      bac.classList.add("bac-btn");
+      main.appendChild(bac);
     }
-    const sec = document.createElement("section");
-    const div = document.createElement("div");
-    const div2 = document.createElement("div");
-    const h1 = document.createElement("h1");
-    const bnt_up = document.createElement("button");
-    const bnt_down = document.createElement("button");
-    
-    sec.classList.add("floor");
-    sec.setAttribute("id",`floor-${i + 1}"`)
-    div.classList.add("floor-section");
-    div2.classList.add("btn-section");
-    h1.textContent = `floor ${i + 1}`;
-    bnt_up.setAttribute("id",`up-${i+1}`)
-    bnt_down.setAttribute("id",`down-${i+1}`)
-    bnt_up.classList.add("btn" , "btn-up");
-    bnt_down.classList.add("btn", "btn-down");
-    bnt_up.textContent = "Up";
-    bnt_down.textContent = "Down";
-    
-    main.appendChild(sec);
-    sec.appendChild(div);
-    div.appendChild(div2);
-    div2.appendChild(h1);
-    div2.appendChild(bnt_up);
-    div2.appendChild(bnt_down);
 
-    for (j = 0; j < input2.value; j++) {
+    const floorSection = document.createElement("section");
+    const floorDiv = document.createElement("div");
+    const btnDiv = document.createElement("div");
+    const floorHeading = document.createElement("h1");
+    const upBtn = document.createElement("button");
+    const downBtn = document.createElement("button");
+
+    floorSection.classList.add("floor");
+    floorSection.setAttribute("id", `floor-${i + 1}`);
+    floorDiv.classList.add("floor-section");
+    btnDiv.classList.add("btn-section");
+    floorHeading.textContent = `floor ${i + 1}`;
+    upBtn.setAttribute("id", `up-${i + 1}`);
+    downBtn.setAttribute("id", `down-${i + 1}`);
+    upBtn.classList.add("btn", "btn-up");
+    downBtn.classList.add("btn", "btn-down");
+    upBtn.textContent = "Up";
+    downBtn.textContent = "Down";
+
+    main.appendChild(floorSection);
+    floorSection.appendChild(floorDiv);
+    floorDiv.appendChild(btnDiv);
+    btnDiv.appendChild(floorHeading);
+    btnDiv.appendChild(upBtn);
+    btnDiv.appendChild(downBtn);
+
+    for (let j = 0; j < input2.value; j++) {
       if (i === 0) {
         const lift = document.createElement("div");
-        const left = document.createElement("span");
-        const right = document.createElement("span");
+        const leftSpan = document.createElement("span");
+        const rightSpan = document.createElement("span");
         lift.classList.add("lift");
-        lift.setAttribute("id",`lift-${j+1}`)
-        left.classList.add("left");
-        right.classList.add("right");
-        lift.appendChild(left);
-        lift.appendChild(right);
-        div.appendChild(lift);
+        lift.setAttribute("id", `lift-${j + 1}`);
+        leftSpan.classList.add("left");
+        rightSpan.classList.add("right");
+        lift.appendChild(leftSpan);
+        lift.appendChild(rightSpan);
+        floorDiv.appendChild(lift);
       }
     }
   }
+
+
     
 
 
-  let currentFloor = 1; // initialize the current floor to 1
+  // let currentFloor = 1; // initialize the current floor to 1
   const upbtns = document.querySelectorAll(".floor .btn-up");
   const downbtns = document.querySelectorAll(".floor .btn-down");
   const floors = document.querySelectorAll(".floor");
+  let currentFloor = 1;
+  let floorNum;
 
-  function onFloorChange(floorNum) {
-    console.log(`Lift moved to floor ${floorNum}`);
-    lift(floorNum);
-  }
-
-  function lift(floorNum=1) {
-    const lift = document.querySelector(".lift");
-    lift.setAttribute("data-current-floor", floorNum);
-    return lift.getAttribute("data-current-floor");
-  }
+function runElevator() {
   
+  lift();
+  moveLift(upbtns, 'up');
+  moveLift(downbtns, 'down');
 
-function moveLift(buttons, direction , onFloorChange) {
-    
+}
+
+function lift(floorNum) {
+  const lifts = document.querySelectorAll('.lift');
+  
+  lifts.forEach((lift, index,floorNum) => {
+    const floorno =Array.from(floorNum)[1]
+    console.log(`lif ${floorno}`)
+    console.log(lift)
+    lift.setAttribute('data-current-floor', currentFloor);
+  });
+}
+
+function moveLift(buttons, direction) {
   buttons.forEach((btn, index) => {
-    liftui =lift()
-    btn.addEventListener("click", function() {
-      console.log(liftui)
-      const lift = document.querySelector(`#lift-${liftui}`);
-      const floorNum = btn.id.split("-")[1];
+    
+    btn.addEventListener('click', function() {
+      const lift = document.querySelector('.lift[data-current-floor]');
+      console.log(lift)
+      floorNum = parseInt(btn.id.split('-')[1]);
       const floor = Array.from(floors)[index];
       const floorHeight = floor.offsetHeight + 5;
 
       if (direction === 'up' && currentFloor < floorNum) {
         lift.style.transform = `translateY(${-(floorHeight) * (floorNum - 1)}px)`;
+        console.log(`The elevator has arrived from floor ${currentFloor} at floor ${floorNum}.`);
         currentFloor = floorNum;
       } else if (direction === 'down' && currentFloor > floorNum) {
         lift.style.transform = `translateY(${-floorHeight * (floorNum - 1)}px)`;
+        console.log(`The elevator has arrived from floor ${currentFloor} at floor ${floorNum}.`);
         currentFloor = floorNum;
       }
-      console.log(liftui)
-      // adding data attribute
-      lift.setAttribute("data-current-floor", floorNum);
-      
-      // call the callback function when the floor changes
-      onFloorChange(floorNum);
+
+      lift.setAttribute('data-current-floor', floorNum);
     });
   });
 }
 
-
-
-moveLift(upbtns, 'up', onFloorChange);
-moveLift(downbtns, 'down', onFloorChange);
-
-
-// function lift(){
-      
-  //   const lifgen = document.querySelectorAll(".lift")
-  //   lifgen.forEach((data-current-floor, floorNum), =>{
-  //     if (dataCurrentFloor === floorNum){
-  //       const liftno = liftno.id.split("-")[1]
-  //     }
-      
-  //     return 
-      
-  //   })
-  // }
-
-
-
+runElevator();
 
 
   //back button 
